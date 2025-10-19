878832f8950252f5f3124ab2f5f0193ceb4c0b6a
Lukas Knöpfel <shylux@gmail.com>
2025-10-19 18:48:21 +0200

    Just saving whatever changes are here. Not worked on it in a while.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import GLSlideshow from './gl-slideshow.module.js';
 
+const googlePhotoPickerEndpoint = 'https://photospicker.googleapis.com';
+
 const slideshow = new GLSlideshow(
     ['./1.jpeg', './2.jpeg'],
     {
@@ -20,3 +22,38 @@ slideshow.addEventListener('transitionEnd', function () {
 $(window).on("resize", function () {
     slideshow.setSize($(this).width(), $(this).height());
 }).trigger('resize');
+
+// initiate login
+$(document).on('click', '#login', function () {
+    let scope = 'https://www.googleapis.com/auth/photospicker.mediaitems.readonly';
+    let redirectURL = 'http://localhost:63343/GPhotoTraveler/index.html';
+    let clientID = '555416435103-dfnp4ccsrgeb9o6lqvouim2q793ddbcr.apps.googleusercontent.com';
+    window.location = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&response_type=token&redirect_uri=${redirectURL}&client_id=${clientID}`;
+});
+
+// store access token
+let fragment = new URLSearchParams(decodeURIComponent(window.location.hash.substring(1)));
+if (fragment.has('error')) {
+    console.error(fragment.get('error'));
+}
+if (fragment.has('access_token')) {
+    console.log(fragment.get('access_token'));
+    localStorage.setItem("access_token", fragment.get('access_token'));
+}
+
+// select photos
+$(document).on('click', '#select', async function () {
+    if (localStorage.getItem("access_token")) {
+        // we have access!
+        const response = await fetch(`${googlePhotoPickerEndpoint}/v1/sessions`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+                'Accept': 'application/json',
+            },
+        })
+        const jsonResponse = await response.json();
+        localStorage.setItem('session_id', jsonResponse.id);
+        window.open(jsonResponse.pickerUri, '_blank').focus();
+    }
+});
